159964ad158932ab4284c171b3ed6020bf2f1b7d
Arunkumar Palani <arunkumarpas002@gmail.com>
2018-10-06 11:23:42 +0530

    Add files via upload

---
+++ ramani.java
@@ -0,0 +1,15 @@
+class mani
+	{
+		public void display()
+			{
+				System.out.print("ArunKumar");
+			}
+	}
+class ramani
+	{
+		public static void main(String a[])
+			{
+				mani obj=new mani();
+				obj.display();
+			}
+	}
